05b2c4bc76162cdb9400917cdc38bee6e3241717
Sylvester <Sylvesterwadley2004@gmail.com>
2024-10-09 14:11:30 -0400

    origin

---
+++ index.js
@@ -0,0 +1,15 @@
+//First Alert
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`)
+
+const firstComboNumber = 10 * 1;
+const secComboNumber = 42 - 2
+const thirdComboNumber = 35 + 4
+
+console.log(firstComboNumber)
+console.log(secComboNumber)
+console.log(thirdComboNumber)
+
+
+//Second Alert
+alert (`${firstComboNumber}* ${secComboNumber}* ${thirdComboNumber}`)
+
